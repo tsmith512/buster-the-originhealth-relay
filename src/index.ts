daa4a677cd1cf587fbe38ea4b2a8c5b7bcc32d2a
Taylor Smith <tsmith@tsmithcreative.com>
2021-11-16 21:00:26 -0600

    Redirect to the blog home page with a cache buster argument

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,11 @@
-import { handleRequest } from './handler'
+addEventListener('fetch', (event: FetchEvent): void => {
+  event.respondWith(handleRequest(event.request));
+});
 
-addEventListener('fetch', (event) => {
-  event.respondWith(handleRequest(event.request))
-})
+async function handleRequest(request: Request): Promise<Response> {
+  const origin = 'blog.cloudflare.com';
+  const now = Math.floor(Date.now() / 1000);
+  const buster = `https://${origin}/?mrkeng_cache_buster?=${now}`;
+
+  return Response.redirect(buster, 302);
+}
